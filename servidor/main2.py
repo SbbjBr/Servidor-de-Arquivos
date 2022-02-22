a35ff31d7b33718c1984b8a4ad65bfb711c41409
Lucas Rocha <44853524+SbbjBr@users.noreply.github.com>
2022-02-22 09:19:15 -0300

    Deixa main2 mais legivel

--- servidor/main2.py
+++ servidor/main2.py
@@ -1,9 +1,9 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
+        MainWindow.setWindowTitle("Servidor de Arquivos - Lucas Rocha")
         MainWindow.resize(456, 260)
         MainWindow.setMinimumSize(QtCore.QSize(456, 260))
         MainWindow.setMaximumSize(QtCore.QSize(456, 260))
@@ -21,53 +21,6 @@ class Ui_MainWindow(object):
 
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.formLayoutWidget = QtWidgets.QWidget(self.centralwidget)
-        self.formLayoutWidget.setGeometry(QtCore.QRect(170, 20, 231, 61))
-        self.formLayoutWidget.setObjectName("formLayoutWidget")
-        self.formLayout = QtWidgets.QFormLayout(self.formLayoutWidget)
-        self.formLayout.setContentsMargins(0, 0, 0, 0)
-        self.formLayout.setObjectName("formLayout")
-
-
-        self.label_ip = QtWidgets.QLabel(self.formLayoutWidget)
-        self.label_ip.setObjectName("label_ip")
-        self.formLayout.setWidget(0, QtWidgets.QFormLayout.LabelRole, self.label_ip)
-
-
-        self.label_porta = QtWidgets.QLabel(self.formLayoutWidget)
-        self.label_porta.setObjectName("label_porta")
-        self.formLayout.setWidget(1, QtWidgets.QFormLayout.LabelRole, self.label_porta)
-
-
-        self.textEdit_ip = QtWidgets.QTextEdit(self.formLayoutWidget)
-        self.textEdit_ip.setObjectName("textEdit_ip")
-        self.formLayout.setWidget(0, QtWidgets.QFormLayout.FieldRole, self.textEdit_ip)
-
-
-        self.textEdit_porta = QtWidgets.QTextEdit(self.formLayoutWidget)
-        self.textEdit_porta.setObjectName("textEdit_porta")
-        self.formLayout.setWidget(1, QtWidgets.QFormLayout.FieldRole, self.textEdit_porta)
-
-
-        self.label_tempo_aberto_m = QtWidgets.QLabel(self.centralwidget)
-        self.label_tempo_aberto_m.setGeometry(QtCore.QRect(30, 150, 158, 16))
-        self.label_tempo_aberto_m.setObjectName("label_tempo_aberto_m")
-
-        self.botao_parar = QtWidgets.QPushButton(self.centralwidget)
-        self.botao_parar.setGeometry(QtCore.QRect(10, 220, 158, 24))
-        self.botao_parar.setObjectName("botao_parar")
-
-        self.label_status = QtWidgets.QLabel(self.centralwidget)
-        self.label_status.setGeometry(QtCore.QRect(230, 150, 239, 16))
-        self.label_status.setObjectName("label_status")
-
-        self.label_ip_botao = QtWidgets.QLabel(self.centralwidget)
-        self.label_ip_botao.setGeometry(QtCore.QRect(230, 180, 239, 16))
-        self.label_ip_botao.setObjectName("label_ip_botao")
-
-        self.botao_iniciar = QtWidgets.QPushButton(self.centralwidget)
-        self.botao_iniciar.setGeometry(QtCore.QRect(200, 220, 239, 24))
-        self.botao_iniciar.setObjectName("botao_iniciar")
 
         self.formLayoutWidget_2 = QtWidgets.QWidget(self.centralwidget)
         self.formLayoutWidget_2.setGeometry(QtCore.QRect(20, 100, 411, 31))
@@ -77,14 +30,105 @@ class Ui_MainWindow(object):
         self.formLayout_2.setContentsMargins(0, 0, 0, 0)
         self.formLayout_2.setObjectName("formLayout_2")
 
-        self.textEdit_caminho = QtWidgets.QTextEdit(self.formLayoutWidget_2)
-        self.textEdit_caminho.setObjectName("textEdit_caminho")
-        self.formLayout_2.setWidget(0, QtWidgets.QFormLayout.FieldRole, self.textEdit_caminho)
+        
+        self.formLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.formLayoutWidget.setGeometry(QtCore.QRect(170, 20, 231, 61))
+        self.formLayoutWidget.setObjectName("formLayoutWidget")
+        self.formLayout = QtWidgets.QFormLayout(self.formLayoutWidget)
+        self.formLayout.setContentsMargins(0, 0, 0, 0)
+        self.formLayout.setObjectName("formLayout")
+
+        
+        #=================================== Label imutaveis =====================================
+
+        self.label_ip = QtWidgets.QLabel(self.formLayoutWidget)
+        self.label_ip.setObjectName("label_ip")
+        self.formLayout.setWidget(0, QtWidgets.QFormLayout.LabelRole, self.label_ip)
+        self.label_ip.setText("IP:")
+
+
+        self.label_porta = QtWidgets.QLabel(self.formLayoutWidget)
+        self.label_porta.setObjectName("label_porta")
+        self.formLayout.setWidget(1, QtWidgets.QFormLayout.LabelRole, self.label_porta)
+        self.label_porta.setText("Porta:")
+
+        self.label_tempo_aberto_m = QtWidgets.QLabel(self.centralwidget)
+        self.label_tempo_aberto_m.setGeometry(QtCore.QRect(30, 150, 158, 16))
+        self.label_tempo_aberto_m.setObjectName("label_tempo_aberto_m")
+        self.label_tempo_aberto_m.setText("Tempo Servidor Aberto:")
 
         self.label_caminho = QtWidgets.QLabel(self.formLayoutWidget_2)
         self.label_caminho.setObjectName("label_caminho")
         self.formLayout_2.setWidget(0, QtWidgets.QFormLayout.LabelRole, self.label_caminho)
+        self.label_caminho.setText("Caminho:")
 
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(130, 180, 49, 16))
+        self.label.setObjectName("label")
+        self.label.setText("minutos")
+        # ========================================================================================
+
+        #=================================== Label mutaveis ======================================
+        
+        self.label_status = QtWidgets.QLabel(self.centralwidget)
+        self.label_status.setGeometry(QtCore.QRect(230, 150, 239, 16))
+        self.label_status.setObjectName("label_status")
+        self.label_status.setText("Status:         Parado")
+        
+        self.label_ip_botao = QtWidgets.QLabel(self.centralwidget)
+        self.label_ip_botao.setGeometry(QtCore.QRect(230, 180, 239, 16))
+        self.label_ip_botao.setObjectName("label_ip_botao")
+        self.label_ip_botao.setText("IP:")
+        # ========================================================================================
+
+        #=================================== Text edit ======================================
+
+        self.textEdit_ip = QtWidgets.QTextEdit(self.formLayoutWidget)
+        self.textEdit_ip.setObjectName("textEdit_ip")
+        self.formLayout.setWidget(0, QtWidgets.QFormLayout.FieldRole, self.textEdit_ip)
+        self.textEdit_ip.setPlaceholderText("'localhost' ou seu ipv4")
+
+
+        self.textEdit_porta = QtWidgets.QTextEdit(self.formLayoutWidget)
+        self.textEdit_porta.setObjectName("textEdit_porta")
+        self.formLayout.setWidget(1, QtWidgets.QFormLayout.FieldRole, self.textEdit_porta)
+        self.textEdit_porta.setPlaceholderText("8000")
+
+        self.textEdit_caminho = QtWidgets.QTextEdit(self.formLayoutWidget_2)
+        self.textEdit_caminho.setObjectName("textEdit_caminho")
+        self.formLayout_2.setWidget(0, QtWidgets.QFormLayout.FieldRole, self.textEdit_caminho)
+        self.textEdit_caminho.setPlaceholderText("Caminho da pasta especifica ou deixe vazio")
+        # ========================================================================================
+        #=================================== Botão ======================================
+
+        self.botao_parar = QtWidgets.QPushButton(self.centralwidget)
+        self.botao_parar.setGeometry(QtCore.QRect(10, 220, 158, 24))
+        self.botao_parar.setObjectName("botao_parar")
+        self.botao_parar.setText("Parar")
+
+        
+
+        self.botao_iniciar = QtWidgets.QPushButton(self.centralwidget)
+        self.botao_iniciar.setGeometry(QtCore.QRect(200, 220, 239, 24))
+        self.botao_iniciar.setObjectName("botao_iniciar")
+        self.botao_iniciar.setText("Clique para iniciar")
+        # ========================================================================================
+
+        #=================================== Widgets ======================================
+
+        self.spinBox_tempo = QtWidgets.QSpinBox(self.centralwidget)
+        self.spinBox_tempo.setGeometry(QtCore.QRect(35, 50, 81, 22))
+        self.spinBox_tempo.setMaximum(99999)
+        self.spinBox_tempo.setObjectName("spinBox_tempo")
+        self.spinBox_tempo.setDisabled(True)
+
+        
+        self.checkBox = QtWidgets.QCheckBox(self.centralwidget)
+        self.checkBox.setGeometry(QtCore.QRect(20, 20, 131, 20))
+        self.checkBox.setObjectName("checkBox")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.checkBox.setText("Com tempo limite?")
+        
         self.lcdnumero = QtWidgets.QLCDNumber(self.centralwidget)
         self.lcdnumero.setGeometry(QtCore.QRect(40, 180, 81, 23))
         palette = QtGui.QPalette()
@@ -100,6 +144,9 @@ class Ui_MainWindow(object):
         self.lcdnumero.setPalette(palette)
         self.lcdnumero.setObjectName("lcdnumero")
 
+        # ========================================================================================
+
+
 
         self.line = QtWidgets.QFrame(self.centralwidget)
         self.line.setGeometry(QtCore.QRect(-20, 135, 531, 21))
@@ -107,25 +154,6 @@ class Ui_MainWindow(object):
         self.line.setFrameShadow(QtWidgets.QFrame.Sunken)
         self.line.setObjectName("line")
 
-
-        self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(130, 180, 49, 16))
-        self.label.setObjectName("label")
-
-
-        self.spinBox_tempo = QtWidgets.QSpinBox(self.centralwidget)
-        self.spinBox_tempo.setGeometry(QtCore.QRect(35, 50, 81, 22))
-        self.spinBox_tempo.setMaximum(99999)
-        self.spinBox_tempo.setObjectName("spinBox_tempo")
-        self.spinBox_tempo.setDisabled(True)
-
-        
-        self.checkBox = QtWidgets.QCheckBox(self.centralwidget)
-        self.checkBox.setGeometry(QtCore.QRect(20, 20, 131, 20))
-        self.checkBox.setObjectName("checkBox")
-        MainWindow.setCentralWidget(self.centralwidget)
-
-        self.retranslateUi(MainWindow)
         self.botao_iniciar.clicked['bool'].connect(self.textEdit_porta.setEnabled) # type: ignore
         self.botao_iniciar.clicked['bool'].connect(self.textEdit_ip.setEnabled) # type: ignore
         self.botao_parar.clicked['bool'].connect(self.textEdit_porta.setDisabled) # type: ignore
@@ -137,6 +165,7 @@ class Ui_MainWindow(object):
         self.botao_iniciar.clicked['bool'].connect(self.checkBox.setEnabled) # type: ignore
         self.botao_parar.clicked['bool'].connect(self.spinBox_tempo.setDisabled) # type: ignore
         self.botao_parar.clicked['bool'].connect(self.checkBox.setDisabled) # type: ignore
+
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
         MainWindow.setTabOrder(self.textEdit_ip, self.textEdit_porta)
         MainWindow.setTabOrder(self.textEdit_porta, self.checkBox)
@@ -144,21 +173,6 @@ class Ui_MainWindow(object):
         MainWindow.setTabOrder(self.spinBox_tempo, self.textEdit_caminho)
         MainWindow.setTabOrder(self.textEdit_caminho, self.botao_iniciar)
         MainWindow.setTabOrder(self.botao_iniciar, self.botao_parar)
-
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "Servidor de Arquivos - Lucas Rocha"))
-        self.label_ip.setText(_translate("MainWindow", "IP:"))
-        self.label_porta.setText(_translate("MainWindow", "Porta:"))
-        self.label_tempo_aberto_m.setText(_translate("MainWindow", "Tempo Servidor Aberto:"))
-        self.botao_parar.setText(_translate("MainWindow", "Parar"))
-        self.label_status.setText(_translate("MainWindow", "Status:         Parado"))
-        self.label_ip_botao.setText(_translate("MainWindow", "IP:"))
-        self.botao_iniciar.setText(_translate("MainWindow", "Clique para iniciar"))
-        self.label_caminho.setText(_translate("MainWindow", "Caminho:"))
-        self.label.setText(_translate("MainWindow", "minutos"))
-        self.checkBox.setText(_translate("MainWindow", "Com tempo limite?"))
-
 
 if __name__ == "__main__":
     import sys
